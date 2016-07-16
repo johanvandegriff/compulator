746a1734a8bb978ad5577747ccc545b407f6edc4
Nikita Wootten <nikita.wootten@gmail.com>
2016-07-16 18:53:43 -0400

    Work on daemonization.
    -PID now prints correctly
    -Daemon is created properly

--- KeyboardDaemon/src/KeyboardDaemon.c
+++ KeyboardDaemon/src/KeyboardDaemon.c
@@ -13,6 +13,23 @@
 #include "KeyboardInterface.h"
 
 int main(void) {
+	// Daemonize the process
+	pid_t pid;
+	pid = fork();
+
+	if(pid < 0) {
+		// Fork was a failure
+		printf("Fork failure! Exiting.");
+		exit(EXIT_FAILURE);
+	}
+
+	if(pid > 0) {
+		// Fork was a success
+		printf("Fork successful! PID: %x\n", pid);
+		exit(EXIT_SUCCESS);
+	}
+
+	// Child process starts here
 	init_keyboard();
 	usleep(100000);
 	for(int i = 0; i < 4; i++) {
@@ -22,5 +39,5 @@ int main(void) {
 		typeKey(KEY_T);
 	}
 	destroy_keyboard();
-	return EXIT_SUCCESS;
+	exit(EXIT_SUCCESS);
 }
